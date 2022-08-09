c40d41e5e76b19eee1bd211909a9f0a14915ce1b
Arun Singh <57107878+arunsingh-me@users.noreply.github.com>
2022-08-09 21:27:56 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,68 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+const cors = require('cors');
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.DATABASE_URL,
+    { useNewUrlParser: true ,useUnifiedTopology: true}
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+// EJS
+app.use(cors());
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+// Express body parser
+//app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'))
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Express session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+app.use('/poll', require('./routes/poll.js'));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
